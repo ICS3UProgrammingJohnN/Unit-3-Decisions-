9518d89a37b497b4506c521e87dc90c3d146bd83
John Ngundeng <36854092+JohnNgundeng@users.noreply.github.com>
2018-10-10 11:12:20 -0400

    yes

--- GuessGameJohnN/GuessGameJohnN/GuessGameForm.Designer.cs
+++ GuessGameJohnN/GuessGameJohnN/GuessGameForm.Designer.cs
@@ -28,20 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.picAnswer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuess.Location = new System.Drawing.Point(12, 52);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(340, 26);
+            this.lblGuess.TabIndex = 0;
+            this.lblGuess.Text = "Guess the correct number between 1-10";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(376, 49);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 33);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(66, 203);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(203, 36);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "You got it right!";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(237, 113);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(74, 31);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check ";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // picAnswer
+            // 
+            this.picAnswer.Image = global::GuessGameJohnN.Properties.Resources.checkmark;
+            this.picAnswer.Location = new System.Drawing.Point(340, 163);
+            this.picAnswer.Name = "picAnswer";
+            this.picAnswer.Size = new System.Drawing.Size(156, 126);
+            this.picAnswer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picAnswer.TabIndex = 4;
+            this.picAnswer.TabStop = false;
             // 
             // frmGuessGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(580, 322);
+            this.Controls.Add(this.picAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblGuess);
             this.Name = "frmGuessGame";
-            this.Text = "Form1";
+            this.Text = "Guessing Game";
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.PictureBox picAnswer;
     }
 }
 
--- GuessGameJohnN/GuessGameJohnN/GuessGameForm.cs
+++ GuessGameJohnN/GuessGameJohnN/GuessGameForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+     * Created by: John Ngundeng
+     * Created on: October 10, 2018
+     * Created for: ICS3U Programming
+     * Daily Assignment – Day #16 - Guessing Game
+     * This program makes the user guess the right number
+    */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +22,35 @@ namespace GuessGameJohnN
         public frmGuessGame()
         {
             InitializeComponent();
+            // Hiding the labels and picture
+            lblAnswer.Hide();
+            picAnswer.Hide();            
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            // declare variables
+            int userGuess;
+            const int CORRECTNUMBER = 3;
+
+            // Get guess from the textbox
+            userGuess = int.Parse(txtGuess.Text);
+
+            // if the guess i
+            if (userGuess == CORRECTNUMBER)
+            {
+                this.picAnswer.Image = Properties.Resources.checkmark;
+                lblAnswer.Text = "Your guess was right!.";
+                lblAnswer.Show();
+                picAnswer.Show();
+            }
+            else
+            {
+                this.picAnswer.Image = Properties.Resources.red_x;
+                lblAnswer.Text = "Your guess was wrong.";
+                lblAnswer.Show();
+                picAnswer.Show();
+            }
         }
     }
 }
