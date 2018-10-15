87e13fec8f871811fb3ca71c1b69dc6fd12243ba
John Ngundeng <36854092+JohnNgundeng@users.noreply.github.com>
2018-10-15 11:13:53 -0400

    ju

--- RockPapSciJohnN/RockPapSciJohnN/RockPapersScissorsForm.Designer.cs
+++ RockPapSciJohnN/RockPapSciJohnN/RockPapersScissorsForm.Designer.cs
@@ -28,20 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.radPlayerRock = new System.Windows.Forms.RadioButton();
+            this.radPlayerPaper = new System.Windows.Forms.RadioButton();
+            this.radPlayerScissors = new System.Windows.Forms.RadioButton();
+            this.radComPaper = new System.Windows.Forms.RadioButton();
+            this.radComRock = new System.Windows.Forms.RadioButton();
+            this.radComScissors = new System.Windows.Forms.RadioButton();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.grbComputerChoice = new System.Windows.Forms.GroupBox();
+            this.grbPlayerChoice = new System.Windows.Forms.GroupBox();
+            this.picComChoice = new System.Windows.Forms.PictureBox();
+            this.picPlayerChoice = new System.Windows.Forms.PictureBox();
+            this.lblOutcome = new System.Windows.Forms.Label();
+            this.grbComputerChoice.SuspendLayout();
+            this.grbPlayerChoice.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picComChoice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayerChoice)).BeginInit();
             this.SuspendLayout();
+            // 
+            // radPlayerRock
+            // 
+            this.radPlayerRock.AutoSize = true;
+            this.radPlayerRock.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radPlayerRock.Location = new System.Drawing.Point(27, 24);
+            this.radPlayerRock.Name = "radPlayerRock";
+            this.radPlayerRock.Size = new System.Drawing.Size(77, 32);
+            this.radPlayerRock.TabIndex = 0;
+            this.radPlayerRock.TabStop = true;
+            this.radPlayerRock.Text = "Rock";
+            this.radPlayerRock.UseVisualStyleBackColor = true;
+            // 
+            // radPlayerPaper
+            // 
+            this.radPlayerPaper.AutoSize = true;
+            this.radPlayerPaper.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radPlayerPaper.Location = new System.Drawing.Point(27, 85);
+            this.radPlayerPaper.Name = "radPlayerPaper";
+            this.radPlayerPaper.Size = new System.Drawing.Size(85, 32);
+            this.radPlayerPaper.TabIndex = 1;
+            this.radPlayerPaper.TabStop = true;
+            this.radPlayerPaper.Text = "Paper";
+            this.radPlayerPaper.UseVisualStyleBackColor = true;
+            // 
+            // radPlayerScissors
+            // 
+            this.radPlayerScissors.AutoSize = true;
+            this.radPlayerScissors.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radPlayerScissors.Location = new System.Drawing.Point(27, 143);
+            this.radPlayerScissors.Name = "radPlayerScissors";
+            this.radPlayerScissors.Size = new System.Drawing.Size(105, 32);
+            this.radPlayerScissors.TabIndex = 2;
+            this.radPlayerScissors.TabStop = true;
+            this.radPlayerScissors.Text = "Scissors";
+            this.radPlayerScissors.UseVisualStyleBackColor = true;
+            // 
+            // radComPaper
+            // 
+            this.radComPaper.AutoSize = true;
+            this.radComPaper.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radComPaper.Location = new System.Drawing.Point(44, 85);
+            this.radComPaper.Name = "radComPaper";
+            this.radComPaper.Size = new System.Drawing.Size(85, 32);
+            this.radComPaper.TabIndex = 3;
+            this.radComPaper.TabStop = true;
+            this.radComPaper.Text = "Paper";
+            this.radComPaper.UseVisualStyleBackColor = true;
+            this.radComPaper.CheckedChanged += new System.EventHandler(this.radComPaper_CheckedChanged);
+            // 
+            // radComRock
+            // 
+            this.radComRock.AutoSize = true;
+            this.radComRock.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radComRock.Location = new System.Drawing.Point(44, 24);
+            this.radComRock.Name = "radComRock";
+            this.radComRock.Size = new System.Drawing.Size(77, 32);
+            this.radComRock.TabIndex = 4;
+            this.radComRock.TabStop = true;
+            this.radComRock.Text = "Rock";
+            this.radComRock.UseVisualStyleBackColor = true;
+            // 
+            // radComScissors
+            // 
+            this.radComScissors.AutoSize = true;
+            this.radComScissors.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radComScissors.Location = new System.Drawing.Point(44, 143);
+            this.radComScissors.Name = "radComScissors";
+            this.radComScissors.Size = new System.Drawing.Size(105, 32);
+            this.radComScissors.TabIndex = 5;
+            this.radComScissors.TabStop = true;
+            this.radComScissors.Text = "Scissors";
+            this.radComScissors.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(257, 282);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(68, 35);
+            this.btnPlay.TabIndex = 6;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // grbComputerChoice
+            // 
+            this.grbComputerChoice.Controls.Add(this.radComRock);
+            this.grbComputerChoice.Controls.Add(this.radComPaper);
+            this.grbComputerChoice.Controls.Add(this.radComScissors);
+            this.grbComputerChoice.Location = new System.Drawing.Point(402, 72);
+            this.grbComputerChoice.Name = "grbComputerChoice";
+            this.grbComputerChoice.Size = new System.Drawing.Size(177, 185);
+            this.grbComputerChoice.TabIndex = 7;
+            this.grbComputerChoice.TabStop = false;
+            this.grbComputerChoice.Text = "Computer";
+            // 
+            // grbPlayerChoice
+            // 
+            this.grbPlayerChoice.BackColor = System.Drawing.Color.Transparent;
+            this.grbPlayerChoice.Controls.Add(this.radPlayerRock);
+            this.grbPlayerChoice.Controls.Add(this.radPlayerPaper);
+            this.grbPlayerChoice.Controls.Add(this.radPlayerScissors);
+            this.grbPlayerChoice.Location = new System.Drawing.Point(19, 72);
+            this.grbPlayerChoice.Name = "grbPlayerChoice";
+            this.grbPlayerChoice.Size = new System.Drawing.Size(177, 185);
+            this.grbPlayerChoice.TabIndex = 8;
+            this.grbPlayerChoice.TabStop = false;
+            this.grbPlayerChoice.Text = "Player";
+            // 
+            // picComChoice
+            // 
+            this.picComChoice.Image = global::RockPapSciJohnN.Properties.Resources.rock;
+            this.picComChoice.Location = new System.Drawing.Point(402, 310);
+            this.picComChoice.Name = "picComChoice";
+            this.picComChoice.Size = new System.Drawing.Size(177, 122);
+            this.picComChoice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picComChoice.TabIndex = 9;
+            this.picComChoice.TabStop = false;
+            // 
+            // picPlayerChoice
+            // 
+            this.picPlayerChoice.Image = global::RockPapSciJohnN.Properties.Resources.scissors;
+            this.picPlayerChoice.Location = new System.Drawing.Point(19, 310);
+            this.picPlayerChoice.Name = "picPlayerChoice";
+            this.picPlayerChoice.Size = new System.Drawing.Size(177, 122);
+            this.picPlayerChoice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPlayerChoice.TabIndex = 10;
+            this.picPlayerChoice.TabStop = false;
+            // 
+            // lblOutcome
+            // 
+            this.lblOutcome.AutoSize = true;
+            this.lblOutcome.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutcome.Location = new System.Drawing.Point(252, 50);
+            this.lblOutcome.Name = "lblOutcome";
+            this.lblOutcome.Size = new System.Drawing.Size(83, 28);
+            this.lblOutcome.TabIndex = 11;
+            this.lblOutcome.Text = "Winner";
+            this.lblOutcome.Click += new System.EventHandler(this.lblOutcome_Click);
             // 
             // frmRockPapersScissors
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 355);
+            this.ClientSize = new System.Drawing.Size(596, 462);
+            this.Controls.Add(this.lblOutcome);
+            this.Controls.Add(this.picPlayerChoice);
+            this.Controls.Add(this.picComChoice);
+            this.Controls.Add(this.grbPlayerChoice);
+            this.Controls.Add(this.grbComputerChoice);
+            this.Controls.Add(this.btnPlay);
             this.Name = "frmRockPapersScissors";
             this.Text = "Rock Paper Scissors Game";
+            this.grbComputerChoice.ResumeLayout(false);
+            this.grbComputerChoice.PerformLayout();
+            this.grbPlayerChoice.ResumeLayout(false);
+            this.grbPlayerChoice.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picComChoice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayerChoice)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radPlayerRock;
+        private System.Windows.Forms.RadioButton radPlayerPaper;
+        private System.Windows.Forms.RadioButton radPlayerScissors;
+        private System.Windows.Forms.RadioButton radComPaper;
+        private System.Windows.Forms.RadioButton radComRock;
+        private System.Windows.Forms.RadioButton radComScissors;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.GroupBox grbComputerChoice;
+        private System.Windows.Forms.GroupBox grbPlayerChoice;
+        private System.Windows.Forms.PictureBox picComChoice;
+        private System.Windows.Forms.PictureBox picPlayerChoice;
+        private System.Windows.Forms.Label lblOutcome;
     }
 }
 
--- RockPapSciJohnN/RockPapSciJohnN/RockPapersScissorsForm.cs
+++ RockPapSciJohnN/RockPapSciJohnN/RockPapersScissorsForm.cs
@@ -16,5 +16,15 @@ namespace RockPapSciJohnN
         {
             InitializeComponent();
         }
+
+        private void radComPaper_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblOutcome_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
